5514eba2643d83cfd14ea13b47db87a64a334696
TheNerdySlayer <lasse-g@energimail.dk>
2024-09-23 12:29:15 +0200

    Seperated the the green and yellow from each other.

---
+++ Gul/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Console.ReadKey reads the pressed key and displays it in the console.");
+            Console.WriteLine("While Console.ReadLine reads the previous line in the console or previous Console.Write commandinput from the user.");
+            Console.ReadKey();
+        }
+    }
+}
